1b245f4068a188a2a103a2d93aae0ffacc73ff44
Steven Duran <stevenduran1@yahoo.com>
2022-12-30 19:12:05 -0800

    Added lines & hidden word to main game project

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to the Bulls & Cows Game!");
+    PrintLine("Press enter to continue.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = "torchwood";
 }
