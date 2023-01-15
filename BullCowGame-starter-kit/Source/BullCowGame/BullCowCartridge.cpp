b9c19a9bab6e41c0ef13a5f2b3c4117aa4a0f9e4
Steven Duran <stevenduran1@yahoo.com>
2023-01-15 02:44:51 -0800

    Programmed basics of core game loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to the Bulls & Cows Game!");
-    PrintLine("Press enter to continue.");
+    PrintLine(TEXT("Welcome to the Bulls & Cows Game!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic number remove!
+    PrintLine(TEXT("Press enter to continue."));
+
+    HiddenWord = TEXT("torchwood");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "torchwood";
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You lose!"));
+    }
 }
