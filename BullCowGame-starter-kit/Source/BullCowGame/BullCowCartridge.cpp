dee9e51a66a5480d3a4bc1c742e78caf7217999a
Steven Duran <stevenduran1@yahoo.com>
2023-04-18 00:43:18 -0700

    Initializing game and adding comments

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,16 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Welcoming the player
     PrintLine(TEXT("Welcome to the Bulls & Cows Game!"));
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic number remove!
     PrintLine(TEXT("Press enter to continue."));
 
-    HiddenWord = TEXT("torchwood");
+    InitGame(); // Setting up game
+
+    // Prompt guess from player
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    // Checking if player guess is correct
 
     if (Input == HiddenWord)
     {
@@ -23,4 +29,17 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         PrintLine(TEXT("You lose!"));
     }
+
+    // Check if input is an isogram, then prompt to guess again
+    // Check if right number of chars, then prompt to guess again
+    // Remove life if guess is incorrect & doesn't have right amount of chars
+    // Check how many lives are remaining; if > 0 prompt user input again & show lives left, else print losing msg & hidden word(?)
+    // Ask user to guess again (or press enter to play again?); if yes (check user input), set lives & hidden word again(?) & prompt guess, else quit game
+
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("torchwood");
+    Lives = 3;
 }
